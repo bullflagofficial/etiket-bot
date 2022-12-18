767751c625b726f8aed6381d620a52db7348494e
Bull Flag <113616752+bullflagofficial@users.noreply.github.com>
2022-12-18 14:10:58 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ spam_chats = []
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
   await event.reply(
-Welcome to TwitterHallelujah Mention Bot 
+Welcome to TwitterHallelujah Mention Bot )
 
 Hey! I can help notify everyone 📢 in the group when someone needs them.
 @TwitterHallelujah
